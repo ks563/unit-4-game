a019d031be1c5b0546e0935f53ca11fb6296858d
ks563 <karen.shea90@gmail.com>
2018-12-16 15:48:30 -0600

    got random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
-var randomNum;
-var Wins;
-var Losses;
+var randomNum; //will store value of random number
+var minNum = 19; //lower bound of random number
+var maxNum = 121; // upper bound of random number
 
-function gameSet() {
-    randomNum = Math.floor(Math.random() * 121) + 1;
+function getRandomNumber() {
+    min = Math.ceil(19);
+    max = Math.floor(121);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+randomNum = getRandomNumber();
+console.log(randomNum);
