469f801ffe0e9fb0c39529922e99e88e584b6f22
ks563 <karen.shea90@gmail.com>
2018-12-16 16:26:49 -0600

    added button html, random number values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
-var randomNum; //will store value of random number
+var magicNum; //will store value of random number
 var minNum = 19; //lower bound of random number
 var maxNum = 121; // upper bound of random number
+var crystalValue1 = Math.floor(Math.random() * 12) + 1;
+var crystalValue2 = Math.floor(Math.random() * 12) + 1;
+var crystalValue3 = Math.floor(Math.random() * 12) + 1;
+var crystalValue4 = Math.floor(Math.random() * 12) + 1;
+
 
 function getRandomNumber() {
     min = Math.ceil(19);
@@ -8,5 +13,11 @@ function getRandomNumber() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-randomNum = getRandomNumber();
-console.log(randomNum);
+//console.log(randomNum);
+
+function gameSet() {
+    $("#magic-number").append(randomNum);
+    getRandomNumber();
+    magicNum = getRandomNumber();
+}
+
