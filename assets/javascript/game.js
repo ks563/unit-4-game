17292da2e14b18418d08857020ae776da5847219
ks563 <karen.shea90@gmail.com>
2018-12-19 21:38:11 -0600

    issues with btns continue, got style better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var magicNum; //will store value of random number
 // var minNum = 19; //lower bound of random number
 // var maxNum = 121; // upper bound of random number
 var crystals;
-var addedNum =0; //this will hold the value of pressed button then be added too so we can append it to the div
+var addedNum = 0; //this will hold the value of pressed button then be added too so we can append it to the div
 
 function getRandomNumber() {
     magicNum = Math.floor(Math.random() * 121) + 19;
@@ -19,26 +19,26 @@ function gameSet() {
 }
 
 function randomNumberCrystals() {
-   return {
-       crystal1: {
-           points: Math.floor(Math.random() * 13) + 1,
-           image: "./assets/images/crystal1.png"
-    
+    return {
+        crystal1: {
+            points: Math.floor(Math.random() * 13) + 1,
+            image: "./assets/images/crystal1.png"
+
         },
         crystal2: {
             points: Math.floor(Math.random() * 13) + 1,
             image: "./assets/images/crystal2.png"
         },
         crystal3: {
-        points: Math.floor(Math.random() * 13) + 1,
-        image: "./assets/images/crystal3.png"
-    
+            points: Math.floor(Math.random() * 13) + 1,
+            image: "./assets/images/crystal3.png"
+
         },
-    crystal4: {
-        points: Math.floor(Math.random() * 13) + 1,
-        image: "./assets/images/crystal4.png"
+        crystal4: {
+            points: Math.floor(Math.random() * 13) + 1,
+            image: "./assets/images/crystal4.png"
         }
-};   
+    };
 }
 
 function createCrystals() {
@@ -47,14 +47,14 @@ function createCrystals() {
         var crystalImg = $("<img alt='image' class='crystal-img'>").attr("src", crystals[crystal].image);
         crystalButton.append(crystalImg);
         $(".btn-group").append(crystalButton);
-    }   
+    }
 }
 
 // this function will append button presses to guessed letter on button click - but is retuning NaN
 function buttonMath(crystal) {
     $(".btn-group").click(function (event) {
-        addedNum += crystals[crystal.attr($(this).data(points))];
-        $("#guessed-number").html(addedNum);
+        addedNum += $(crystals)[crystal.attr("data-name")].points;
+        $("#guessed-number").append(addedNum);
         //console.log(addedNum);
     }
     )
@@ -64,12 +64,13 @@ function checkGame() {
     if (addedNum === magicNum) {
         wins++;
         gameSet();
-    } else if(addedNum > magicNum) {
+    } else if (addedNum > magicNum) {
         losses++;
         gameSet();
-    } 
+    }
 }
 
 gameSet();
 createCrystals();
+buttonMath();
 checkGame();
